3298b801096b728c0df610379b2355e2528c82a7
burwinliu <burwincollege@gmail.com>
2020-08-02 16:15:37 -0700

    Setting up CI

--- rp-frontend/src/components/board.tsx
+++ rp-frontend/src/components/board.tsx
@@ -1,36 +1,15 @@
 import React, { useEffect, useState} from 'react';
-import { Stage, Layer, Circle } from 'react-konva';
-
 import '../styles/board.css'
-
-import {getElements} from "../store/temp"
-
-interface BoardProps {
-    dimX : number;
-    dimY: number;
-}
-export const Board = ({dimX, dimY}: BoardProps) => {
-    console.log(dimX, dimY)
-
-    const [toRender, setToRender] = useState<any>({})
+export const Board = () => {
 
     useEffect(()=>{
-        getElements().then((elements) => {
-            console.log(elements)
-        })
     })
 
     return(
         <div className="board-wrap">
             {/* @ts-ignore: Ignore so ts-script wont mess with the height and width props */}
-            <Stage height={600} width={800}>
-                {
-                    Object.keys(toRender).map((key) => {
-                        console.log(toRender[key])
-                    })
-                    
-                }
-            </Stage>
+            <canvas>
+            </canvas>
         </div>
         
     )
--- rp-frontend/src/views/Main.tsx
+++ rp-frontend/src/views/Main.tsx
@@ -2,66 +2,20 @@ import React, {useState} from 'react';
 
 import '../styles/main.css'
 import { Board } from '../components/board';
-import { 
-    TextField, 
+import {  
     List,
-    ListItem,
-    ListItemText,
-    Button
 } from '@material-ui/core'
 
 export const Main = () => {
-    const [boardDimX, setBoardDimX] = useState<number>(0)
-    const [boardDimY, setBoardDimY] = useState<number>(0)
-    
-    const [dimX, setDimX] = useState<number>(0)
-    const [dimY, setDimY] = useState<number>(0)
-
-    const inputDimX = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setBoardDimX(parseInt(event.target.value) || 0)
-    }
-
-    const inputDimY = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setBoardDimY(parseInt(event.target.value) || 0)
-        
-    }
-
-    const handleSet = () => {
-        setDimX(boardDimX)
-        setDimY(boardDimY)
-    }
-
     return(
         <div className="bodyContent">
             <div className="sidebarContent">
                 <List>
-                    <ListItem className="list-item">
-                        <ListItemText>Dimension of the Board</ListItemText>
-                        <div className="row-dimension">
-                            <TextField
-                                label="Dimension X"
-                                type="number"
-                                value={boardDimX} 
-                                onChange={inputDimX}
-                                className="input-dimension"
-                                style={{ margin: 8 }}
-                            />
-                            <TextField
-                                label="Dimension Y"
-                                type="number"
-                                value={boardDimY} 
-                                onChange={inputDimY}
-                                className="input-dimension"
-                                style={{ margin: 8 }}
-                            />
-                        </div>
-                        <Button onClick={handleSet}>Set Board Size</Button>
-                    </ListItem>
                 </List>
                 
             </div>
             <div className="mainContent">
-                <Board dimX={dimX} dimY={dimY}/>
+                <Board/>
             </div>
         </div>
     )
