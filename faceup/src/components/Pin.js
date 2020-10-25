b3dd5f9aaec2f0e20a75d0b0fb8f2eedafe33054
liubin <liubinw@gmail.com>
2020-10-25 16:34:59 -0700

    rename files and remove unused files

--- faceup/src/components/Pin.js
+++ faceup/src/components/Pin.js
@@ -1,22 +1,47 @@
-// Inside src/components/Button/Button.js
-
-/*
-import React from 'react';
-
-const Button = ({ children, ...rest }) => {
-  return (
-    <button className="button" {...rest}>
-      { children }
-    </button>
-  )
-}
-
-export default Button;
-*/
 
 import React from 'react';
-//import PropTypes from 'prop-types';
+
 import './pin.css';
+
+export class PinParameters {
+  constructor(handleClick,location,pinNumber,gpioNumber,pinType,gpioLevel)  {
+    this._handleClick = handleClick;
+    this._location = location;
+    this._pinNumber = pinNumber;
+    this._gpioNumber = gpioNumber;
+    this._pinType = pinType;
+    this._gpioLevel = gpioLevel;
+  }
+
+  get handleClick() {
+    return this._handleClick;
+  }
+
+  get location() {
+    return this._location;
+  }
+  set location(x) {
+    this._location = x;
+  }
+  get pinNumber() {
+    return this._pinNumber;
+  }
+  get gpioNumber() {
+    return this._gpioNumber;
+  }
+  get pinType() {
+    return this._pinType;
+  }
+  set pinType(x) {
+    this._pinType = x;
+  }
+  get gpioLevel() {
+    return this._gpioLevel;
+  }
+  set gpioLevel(x) {
+    this._gpioLevel = x;
+  }
+}
 
 const PinTag = (props) => 
 {
@@ -58,199 +83,145 @@ class BasePin extends React.Component {
 
   //const LeftPin = ({ pinNumber, gpioNumber, pinType, gpioLevel, ...props }) => {
   //class LeftPin extends React.Component {
-  class LeftPin extends BasePin {
-  
+  class PinNew extends BasePin {
+    onClick(clickButton){
+      this.props.parameters.handleClick(this.props.parameters,clickButton);
+      //alert(clickButton);
+    }
+
+    left_pin_render1=(functionID,pinType,descID,gpioNumber,gpioLevel,gpioID,pinNumber)=>
+    {
+      return (
+        <PinTag>
+          <td align="center">
+              <button type="button" className="FunctionBasic" id={functionID} value={pinType} onClick={this.onClick.bind(this,"direction")}>{pinType}</button>
+          </td>
+          <td align="right">
+              <div className="Description" id={descID}>GPIO {gpioNumber}</div>
+          </td>
+          <td align="center">
+              <button type="button" className={gpioLevel} id={gpioID} onClick={this.onClick.bind(this,"pin")}>{pinNumber}</button>
+          </td>
+        </PinTag> 
+        )
+    }
+
+    left_pin_render2=(functionID,pinType,descID,gpioNumber,gpioLevel,gpioID,pinNumber)=>
+    {
+      var pinLevel = "HIGH";
+      if(pinType==="GROUND")
+      {
+        pinLevel = "LOW";
+      }
+      else if(pinType==="--")
+      {
+        pinLevel = "DNC";
+      }
+      else if(this.props.pinType==="5.0V")
+      {
+        pinLevel = "V50";
+      }
+      return (
+        <PinTag>
+          <td align="center"></td>
+          <td align="right">
+              <div className="Description" id={descID}>{pinType}</div>
+          </td>
+          <td align="center">
+              <button type="button" className={pinLevel} id={gpioID} onClick={this.onClick.bind(this,"pin")}>{pinNumber}</button>
+          </td>
+        </PinTag> 
+      )
+    }
+
+    right_pin_render1=(functionID,pinType,descID,gpioNumber,gpioLevel,gpioID,pinNumber)=>
+    {
+      return (
+        <PinTag>
+            <td align="center">
+                <button type="button" className={gpioLevel} id={gpioID} onClick={this.onClick.bind(this,"pin")}>{pinNumber}</button>
+            </td>
+            <td align="left">
+                <div className="Description" id={descID}>GPIO {gpioNumber}</div>
+            </td>
+            <td align="center">
+                <button type="button" className="FunctionBasic" id={functionID} value={pinType} onClick={this.onClick.bind(this,"direction")}>{pinType}</button>
+            </td>       
+          </PinTag> 
+        )
+    }
+
+    right_pin_render2=(functionID,pinType,descID,gpioNumber,gpioLevel,gpioID,pinNumber)=>
+    {
+      var pinLevel = "HIGH";
+      if(pinType==="GROUND")
+      {
+        pinLevel = "LOW";
+      }
+      else if(pinType==="--")
+      {
+        pinLevel = "DNC";
+      }
+      else if(this.props.pinType==="5.0V")
+      {
+        pinLevel = "V50";
+      }
+      return (
+        <PinTag>
+          <td align="center">
+              <button type="button" className={pinLevel} id={gpioID} onClick={this.onClick.bind(this,"pin")}>{pinNumber}</button>
+          </td>
+          <td align="left">
+              <div className="Description" id={descID}>{pinType}</div>
+          </td>
+          <td align="center"></td>
+        </PinTag>
+      )
+    }
+
     render(){
-      const {pinNumber, gpioNumber, pinType, gpioLevel} = this.props;
+      const {location, pinNumber, gpioNumber, pinType, gpioLevel} = this.props.parameters;
       //const descID = "description"+`${gpioNumber}`;
       const descID = `description${gpioNumber}`;
       const functionID = `function${gpioNumber}`;
       const gpioID = `gpio${gpioNumber}`;
 
       //console.log("functionID:",descID);
-
-      switch(pinType){
-        case 'OUT':
-        case 'IN':
-        case 'GPIO':
-          return (
-          <PinTag>
-            <td align="center">
-                <button type="button" className="FunctionBasic" id={functionID} value={pinType}>{pinType}</button>
-            </td>
-            <td align="right">
-                <div className="Description" id={descID}>GPIO {gpioNumber}</div>
-            </td>
-            <td align="center">
-                <button type="button" className={gpioLevel} id={gpioID} onClick={this.togglePinState}>{pinNumber}</button>
-            </td>
-          </PinTag> 
-          )
-        case 'GROUND':
-        case '--':
-        case '3.3V':
-        case '5.0V':  
-          var pinLevel = "HIGH";
-          if(pinType==="GROUND")
-          {
-            pinLevel = "LOW";
-          }
-          else if(pinType==="--")
-          {
-            pinLevel = "DNC";
-          }
-          else if(this.props.pinType==="5.0V")
-          {
-            pinLevel = "V50";
-          }
-          return (
-            <PinTag>
-              <td align="center"></td>
-              <td align="right">
-                  <div className="Description" id={descID}>{pinType}</div>
-              </td>
-              <td align="center">
-                  <button type="button" className={pinLevel} id={gpioID}>{pinNumber}</button>
-              </td>
-            </PinTag> 
-            )
-        default:
-          return null;
+      if(location==="left"){
+        switch(pinType){
+          case 'OUT':
+          case 'IN':
+          case 'GPIO':
+            return this.left_pin_render1(functionID,pinType,descID,gpioNumber,gpioLevel,gpioID,pinNumber);
+          case 'GROUND':
+          case '--':
+          case '3.3V':
+          case '5.0V':  
+            return this.left_pin_render2(functionID,pinType,descID,gpioNumber,gpioLevel,gpioID,pinNumber);
+          default:
+            return null;
+        }
+      }
+      if(location==="right"){
+        switch(pinType){
+          case 'OUT':
+          case 'IN':
+          case 'GPIO':
+            return this.right_pin_render1(functionID,pinType,descID,gpioNumber,gpioLevel,gpioID,pinNumber);
+          case 'GROUND':
+          case '--':
+          case '3.3V':
+          case '5.0V':  
+            return this.right_pin_render2(functionID,pinType,descID,gpioNumber,gpioLevel,gpioID,pinNumber);
+          default:
+            return null;
+        }
+      }
+      else{
+        return "pin location not defined correctly";
       }
     }
   }
-
-  //const RightPin = ({pinNumber, gpioNumber, pinType, gpioLevel, ...props}) => {
-  class RightPin extends React.Component {
-    //const {pinNumber, gpioNumber, pinType, gpioLevel, ...props} = this.props;
-
-    render(){
-      const {pinNumber, gpioNumber, pinType, gpioLevel} = this.props;
-      const descID = `description${this.props.gpioNumber}`;
-      const functionID = `function${gpioNumber}`;
-      const gpioID = `gpio${gpioNumber}`;
-      
-      switch(pinType){
-        case 'OUT':
-        case 'IN':  
-        case 'GPIO':
-          return (
-            <PinTag>
-                <td align="center">
-                    <button type="button" className={gpioLevel} id={gpioID}>{pinNumber}</button>
-                </td>
-                <td align="left">
-                    <div className="Description" id={descID}>GPIO {gpioNumber}</div>
-                </td>
-                <td align="center">
-                    <button type="button" className="FunctionBasic" id={functionID} value={pinType}>{pinType}</button>
-                </td>       
-            </PinTag> 
-            )
-        case 'GROUND':
-        case '--':
-        case '3.3V':
-        case '5.0V':  
-          var pinLevel = "HIGH";
-          if(pinType==="GROUND")
-          {
-            pinLevel = "LOW";
-          }
-          else if(pinType==="--")
-          {
-            pinLevel = "DNC";
-          }
-          else if(this.props.pinType==="5.0V")
-          {
-            pinLevel = "V50";
-          }
-          return (
-            <PinTag>
-              <td align="center">
-                  <button type="button" className={pinLevel} id={gpioID}>{pinNumber}</button>
-              </td>
-              <td align="left">
-                  <div className="Description" id={descID}>{pinType}</div>
-              </td>
-              <td align="center"></td>
-            </PinTag> 
-            )
-        default:
-          return null;
-      }
-    }
-  }
-
-class TestPin extends React.Component {
-  render(){
-    const descID = `description${this.props.gpioNumber}`;
-    const functionID = `function${this.props.gpioNumber}`;
-    const gpioID = `gpio${this.props.gpioNumber}`;
-    
-    switch(this.props.pinType){
-      case 'OUT':
-      case 'IN':
-      case "GPIO":
-        return (
-          <PinTag>
-              <td align="center">
-                  <button type="button" className={this.props.gpioLevel} id={this.props.gpioID}>{this.props.pinNumber}</button>
-              </td>
-              <td align="left">
-                  <div className="Description" id={this.props.descID}>GPIO {this.props.gpioNumber}</div>
-              </td>
-              <td align="center">
-                  <button type="button" className="FunctionBasic" id={functionID} value={this.props.pinType}>{this.props.pinType}</button>
-              </td>       
-          </PinTag> 
-          )
-      case 'GROUND':
-      case '--':
-      case '3.3V':
-      case '5.0V':  
-        var pinLevel = "HIGH";
-        if(this.props.pinType==="GROUND")
-        {
-          pinLevel = "LOW";
-        }
-        else if(this.props.pinType==="--")
-        {
-          pinLevel = "DNC";
-        }
-        else if(this.props.pinType==="5.0V")
-        {
-          pinLevel = "V50";
-        }
-        return (
-          <PinTag>
-            <td align="center">
-                <button type="button" className={pinLevel} id={gpioID}>{this.props.pinNumber}</button>
-            </td>
-            <td align="left">
-                <div className="Description" id={descID}>{this.props.pinType}</div>
-            </td>
-            <td align="center"></td>
-          </PinTag> 
-          )
-      default:
-        return null;
-    }
-  }
-}
-
-export class Pin extends React.Component {
-  render() {
-    return (
-      <PinTag>   
-        {this.props.location==="left"?<LeftPin  {...this.props}/>:
-        this.props.location==="right"?<RightPin {...this.props}/>:
-        this.props.location==="test"?<TestPin {...this.props}/>:
-        "pin location not defined"}
-      </PinTag>
-    );
-    
-  }
-}
 
 /*****************************************************
  * Input parameters:
@@ -272,96 +243,5 @@ export class Pin extends React.Component {
  *    HIGH
  *    LOW
 */
-export class PinTest extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: {},
-    }
-    
-  }
-  
-  componentDidMount() {
-    const url = "http://192.168.0.25:8080/api_request/12/on";
-    //console.log("componentDidMount,the url is " + url);
-    fetch(url)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          //console.log("print out the result");
-          //console.log(result["0"]);
-          //console.log(result[0]);
-          this.setState({
-            isLoaded: true,
-            items: result,
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          console.log("error fetch");
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
 
-  /*
-        <ul>
-          {items.map(item => (
-            <li key={item.name}>
-              {item.name} {item.state}
-            </li>
-          ))}
-      </ul>
-  */
-
- 
-
-  render() {
-    //const { error, isLoaded, items } = this.state;
-    //const { items } = this.state;
-    return (
-      <div id="content" align="center">
-      <table id="RPiHeader">
-      <tbody>
-        <tr>
-          <Pin location="left" pinNumber="25" gpioNumber="27" pinType="GPIO" gpioLevel="HIGH"/>
-          <Pin location="right" pinNumber="12" gpioNumber="15" pinType="3.3V" gpioLevel="LOW"/>
-        </tr>
-        <tr>
-        <Pin location="left" pinNumber="25" gpioNumber="27" pinType="GPIO" gpioLevel="HIGH"/>
-          <Pin location="right" pinNumber="12" gpioNumber="15" pinType="3.3V" gpioLevel="LOW"/>
-        </tr>
-      </tbody>
-      </table>
-      </div>
-    );
-    
-  }
-}
-  
-
-  /*
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
-*/
-
-
-export default Pin;
+export default PinNew;
