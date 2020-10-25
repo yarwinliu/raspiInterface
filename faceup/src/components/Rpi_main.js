e5c41e5f01a595f564392aa5120012bed8d06f1d
liubin <liubinw@gmail.com>
2020-10-25 09:46:26 -0700

    use an array of pair data to construct the rpi main board

--- faceup/src/components/Rpi_main.js
+++ faceup/src/components/Rpi_main.js
@@ -1,6 +1,5 @@
 // Inside src/components/Rpi_main.js
 
-
 import React from 'react';
 import './rpi_main.css';
 import PinNew,{PinParameters}  from "./PinNew";
@@ -12,13 +11,20 @@ class RpiMain extends React.Component {
       error: null,
       isLoaded: false,
       items: {},
-      pins: {},
+      pins: [],
     }
+
+    /* define pins: */
+    /* pinNumber, gpioNumber, pinType, gpioLevel */
+    this.pinData = [
+      [["7","4","IN","HIGH"],["8","5","OUT","LOW"]],
+      [["9","6","IN","HIGH"],["10","7","OUT","LOW"]],
+    ];
   }
   
   handleClick(parameters,clickButton) {
     if(clickButton==="direction"){
-      alert("click button: pin number["+ parameters.pinNumber + "]," + "dirction[" + parameters.pinType + "]");
+      alert("click button: pin number["+ parameters.pinNumber + "],dirction[" + parameters.pinType + "]");
     }
     else if(clickButton==="pin"){
       alert("click button: pin number["+ parameters.pinNumber + "]" );
@@ -26,26 +32,38 @@ class RpiMain extends React.Component {
     else{
       alert("click button: un-supported button type");
     }
-
-    
   }
 
   render() {
-    let pinp1 = new PinParameters(this.handleClick,"left","7","4","IN","HIGH");
-    let pinp2 = new PinParameters(this.handleClick,"right","8","5","OUT","LOW");
+    for (var i = 0; i < this.pinData.length; i++) {
+      //console.log(this.pinData[i][0]);
+      let pl = this.pinData[i][0];
+      let pr = this.pinData[i][1];
+      let pinLeft = new PinParameters(this.handleClick,"left",pl[0],pl[1],pl[2],pl[3]);
+      let pinRight = new PinParameters(this.handleClick,"right",pr[0],pr[1],pr[2],pr[3]);
+      let pinPair = [pinLeft,pinRight];
+      this.state.pins.push(pinPair);
+    }
+
+    const pinArray = this.state.pins.map((pinPair,index) => {
+        const pinLeft = pinPair[0];
+        const pinRight = pinPair[1];
+        return (
+          <tr key={index}>
+            <PinNew parameters={pinLeft}/>
+            <PinNew parameters={pinRight}/>
+          </tr>
+        );
+    });
+
     return (
       <table>
       <tbody>
-      <tr>
-          <PinNew parameters={pinp1}/>
-          <PinNew parameters={pinp2}/>
-      </tr>
+        {pinArray}
       </tbody>
       </table>
     );
   }
-
-  
 }
 
 export default RpiMain;
