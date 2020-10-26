b3fa355ebb5e499d7eb0c7095b8d9b033849855e
liubin <liubinw@gmail.com>
2020-10-26 05:46:23 +0000

    add support for the gpio mode(in/out)

--- rp-backend/rpi_control.py
+++ rp-backend/rpi_control.py
@@ -13,33 +13,37 @@ GPIO.setmode(GPIO.BCM)
 
 # Create a dictionary called pins to store the pin number, name, and pin state:
 pins = {
-    0 : {'name' : 'pin27', 'state' : GPIO.LOW},
-    1 : {'name' : 'pin28', 'state' : GPIO.LOW},
-    2 : {'name' : 'pin3', 'state' : GPIO.LOW},
-    3 : {'name' : 'pin5', 'state' : GPIO.LOW},
-    4 : {'name' : 'pin7', 'state' : GPIO.LOW},
-    5 : {'name' : 'pin29', 'state' : GPIO.LOW},
-    6 : {'name' : 'pin31', 'state' : GPIO.LOW},
+    0 : {'name' : 'pin27', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+    1 : {'name' : 'pin28', 'state' : GPIO.LOW, 'mode' : GPIO.IN},
+    2 : {'name' : 'pin3', 'state' : GPIO.LOW, 'mode' : GPIO.IN},
+    3 : {'name' : 'pin5', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+    4 : {'name' : 'pin7', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+    5 : {'name' : 'pin29', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+    6 : {'name' : 'pin31', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
 
-    7 : {'name' : 'pin26', 'state' : GPIO.LOW},
-    8 : {'name' : 'pin24', 'state' : GPIO.LOW},
-    9 : {'name' : 'pin21', 'state' : GPIO.LOW},
-   10 : {'name' : 'pin19', 'state' : GPIO.LOW},
-   11 : {'name' : 'pin23', 'state' : GPIO.LOW},
-   12 : {'name' : 'pin32', 'state' : GPIO.LOW},
-   13 : {'name' : 'pin33', 'state' : GPIO.LOW},
-   14 : {'name' : 'pin8', 'state' : GPIO.LOW},
+    7 : {'name' : 'pin26', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+    8 : {'name' : 'pin24', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+    9 : {'name' : 'pin21', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+   10 : {'name' : 'pin19', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+   11 : {'name' : 'pin23', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+   12 : {'name' : 'pin32', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+   13 : {'name' : 'pin33', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+   14 : {'name' : 'pin8', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
 
-   15 : {'name' : 'pin10', 'state' : GPIO.LOW},
-   16 : {'name' : 'pin36', 'state' : GPIO.LOW},
-   17 : {'name' : 'pin11', 'state' : GPIO.LOW},
-   18 : {'name' : 'pin12', 'state' : GPIO.LOW},
+   15 : {'name' : 'pin10', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+   16 : {'name' : 'pin36', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+   17 : {'name' : 'pin11', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
+   18 : {'name' : 'pin12', 'state' : GPIO.LOW, 'mode' : GPIO.OUT},
    }
 
 # Set each pin as an output and make it low:
 for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
+   pins[pin]['mode'] = GPIO.gpio_function(pin)
+   if(pins[pin]['mode']==GPIO.OUT):
+      GPIO.setup(pin, GPIO.OUT)
+      GPIO.output(pin, GPIO.LOW)
+   else :
+      GPIO.setup(pin, GPIO.IN)
 
 @app.route("/")
 def main():
@@ -104,10 +108,20 @@ def apiGetAction(changePin, action):
       GPIO.output(changePin, GPIO.LOW)
    if action == "toggle":
       # Read the pin and set it to whatever it isn't (that is, toggle it):
-      GPIO.output(changePin, not GPIO.input(changePin))
-
+      func = GPIO.gpio_function(changePin)
+      if(func==GPIO.OUT):
+        GPIO.output(changePin, not GPIO.input(changePin))
+   if action == "cmode":
+         # change mode: read in pin mode then toggle it
+         func = GPIO.gpio_function(changePin)
+         if(func==GPIO.OUT):
+            GPIO.setup(changePin, GPIO.IN)
+         if(func==GPIO.IN):
+            GPIO.setup(changePin, GPIO.OUT)
+         
    for pin in pins:
       pins[pin]['state'] = GPIO.input(pin)
+      pins[pin]['mode'] = GPIO.gpio_function(pin)
    return jsonify(pins)
 
 if __name__ == "__main__":
