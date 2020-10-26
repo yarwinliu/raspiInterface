a7804157a3ca180ca222463f0d4cf7b116bd34ab
liubin <liubinw@gmail.com>
2020-10-25 22:47:47 -0700

    add the data retrieval functions, finally

--- faceup/src/components/rpiHeader.js
+++ faceup/src/components/rpiHeader.js
@@ -1,5 +1,9 @@
 // Inside src/components/rpiHeader.js
 
+// TODO: 
+//     1. auto refresh
+//     2. move pin data to a json file
+
 import React from 'react';
 import './pin.css';
 import Pin,{PinParameters}  from "./Pin";
@@ -17,19 +21,30 @@ class RpiHeader extends React.Component {
     /* pinNumber, functionNumber, pinType, pinMOde, pinLevel */
     this.pinData = [
       [["11","17","GPIO","IN","HIGH"],["12","18","GPIO","OUT","LOW"]],
+      [["31","6","GPIO","IN","HIGH"],["32","12","GPIO","OUT","LOW"]],
     ];
     //[["9","6","IN","HIGH"],["10","7","OUT","LOW"]],
 
     /* define url */
-    this.url = "http://192.168.0.25:8080/api_request/1/on";
+    this.urlPre = "http://192.168.0.25:8080/api_request/";
 
     this.isRendered = false;
     this.pins = [];
   }
   
-  componentDidMount() {
-    const url = this.url;
-    //console.log("componentDidMount,the url is " + url);
+  pin2bcm = (pinNumber) =>{
+    const { items } = this.state;
+
+    for(var i=0;i<Object.keys(items).length;++i){
+      //console.log(items[i].name);
+      if(("pin"+pinNumber)===items[i].name){  
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  sendData = (url) =>{
     fetch(url)
       .then(res => res.json())
       .then(
@@ -55,12 +70,25 @@ class RpiHeader extends React.Component {
       )
   }
 
+  componentDidMount() {
+    const url = this.urlPre+"0/null";
+    this.sendData(url);
+  }
+
   handleClick(parameters,clickButton) {
     if(clickButton==="direction"){
-      alert("click button: pin number["+ parameters.pinNumber + "],dirction[" + parameters.pinType + "]");
+      console.log("click button: pin number["+ parameters.pinNumber + "],dirction[" + parameters.pinMode + "]");
+      let bcm = this.pin2bcm(parameters.pinNumber);
+      let url = this.urlPre+`${bcm}/cmode`;
+      console.log(url);
+      this.sendData(url);
     }
     else if(clickButton==="pin"){
-      alert("click button: pin number["+ parameters.pinNumber + "]" );
+      console.log("click button: pin number["+ parameters.pinNumber + "]" );
+      let bcm = this.pin2bcm(parameters.pinNumber);
+      let url = this.urlPre+`${bcm}/toggle`;
+      console.log(url);
+      this.sendData(url);
     }
     else{
       alert("click button: un-supported button type");
@@ -76,8 +104,11 @@ class RpiHeader extends React.Component {
           //console.log(this.pinData[i][0]);
           let pl = this.pinData[i][0];
           let pr = this.pinData[i][1];
-          let pinLeft = new PinParameters(this.handleClick,"left",pl[0],pl[1],pl[2],pl[3],pl[4]);
-          let pinRight = new PinParameters(this.handleClick,"right",pr[0],pr[1],pr[2],pr[3],pr[4]);
+          // node: have to add the function as below, or it will not be able to access
+          //       this property. and match the parameters as well
+          let func = (parameters,clickButton)=>this.handleClick(parameters,clickButton);
+          let pinLeft = new PinParameters(func,"left",pl[0],pl[1],pl[2],pl[3],pl[4]);
+          let pinRight = new PinParameters(func,"right",pr[0],pr[1],pr[2],pr[3],pr[4]);
           let pinPair = [pinLeft,pinRight];
           this.pins.push(pinPair);
           this.isRendered = true;
@@ -85,7 +116,7 @@ class RpiHeader extends React.Component {
       }
 
       //update pins array with loaded data in items 
-      //console.log("data loaded, received data length: " + Object.keys(items).length);
+      console.log("data loaded, received data length: " + Object.keys(items).length);
       //console.log("pin data length: " + this.pins.length);
       for(i=0;i<Object.keys(items).length;++i){
         //console.log(items[i].name);
@@ -103,6 +134,12 @@ class RpiHeader extends React.Component {
               else if(items[i].state===1){
                 pinLeft.pinLevel="HIGH";
               }
+              if(items[i].mode===0){      // mode: OUT
+                pinLeft.pinMode="OUT";
+              }
+              else if(items[i].mode===1){
+                pinLeft.pinMode="IN";
+              }
             }
           }
 
@@ -115,11 +152,16 @@ class RpiHeader extends React.Component {
               else if(items[i].state===1){
                 pinRight.pinLevel="HIGH";
               }
+              if(items[i].mode===0){      // mode: OUT
+                pinRight.pinMode="OUT";
+              }
+              else if(items[i].mode===1){
+                pinRight.pinMode="IN";
+              }
             }
           }
         }
       }
-
     }
 
     const pinArray = this.pins.map((pinPair,index) => {
